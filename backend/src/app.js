e8354afe7f0101d49d3b4d87b48ca2320ffe4aef
DuyAnh <duyanhvip3k@gmail.com>
2026-03-12 01:58:28 +0700

    sua api

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
 
 // Middleware
+app.use(
+	cors({
+		origin: process.env.FRONTEND_URL || "http://localhost:3000",
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
